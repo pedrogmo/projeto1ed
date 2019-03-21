ec02510e9c868d0d459081efd84672153f728d54
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-03-21 13:47:22 -0300

    Fim da Classe Celula e começo da ListaCircular

---
+++ 18172_18174_Projeto1ED/Programa/Programa/Celula.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    class Celula
+    {
+        double valor;
+        int linha, coluna;
+        Celula direita, abaixo;
+
+        public Celula(double v, int l, int c, Celula d, Celula a)
+        {
+            Valor = v;
+            linha = l;
+            coluna = c;
+            Direita = d;
+            Abaixo = a;
+        }
+
+        public double Valor
+        {
+            get => valor;
+            set => valor = value;       
+        }
+
+        public int Linha { get => linha; }
+
+        public int Coluna { get => coluna; }
+
+        public Celula Direita
+        {
+            get => direita;
+            set => direita = value;
+        }
+
+        public Celula Abaixo
+        {
+            get => abaixo;
+            set => abaixo = value;
+        }
+
+        public int CompareTo(Celula cel)
+        {
+            if (linha != cel.linha)
+                return linha - cel.linha;
+            return coluna - cel.coluna;
+        }
+    }
+}
---
+++ 18172_18174_Projeto1ED/Programa/Programa/ListaCircularCelula.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    class ListaCircularCelula
+    {
+        Celula cabeca, final;
+        int qtasCelulas;
+
+        public ListaCircularCelula()
+        {
+            cabeca = final = null;
+            qtasCelulas = 0;
+        }
+
+        public int QtasCelulas { get => qtasCelulas; }
+        public Celula Cabeca { get => cabeca; }
+        public Celula Final { get => final; }
+
+        public bool EstaVazia { get => cabeca == null; }
+
+        public void InserirDireita(Celula c)
+        {
+            if (c == null)
+                throw new Exception("Célula nula");
+            if (EstaVazia)
+                cabeca = c;
+            final.Direita = c;
+            c.Direita = cabeca;
+            final = c;
+            qtasCelulas++;
+        }
+
+        public void InserirAbaixo(Celula c)
+        {
+            if (c == null)
+                throw new Exception("Célula nula");
+            if (EstaVazia)
+                cabeca = c;
+            final.Abaixo = c;
+            c.Abaixo = cabeca;
+            final = c;
+            qtasCelulas++;
+        }
+    }
+}
