3485cbcbfb2f410209d3c5dd0fedb533cce4bb68
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-03-25 09:16:51 -0300

    Revert "Esqueleto classe Matriz Esparsa"
    This reverts commit 5a6a930e2ee23029cf95a36a7b2a400a2fc1caa4.

--- 18172_18174_Projeto1ED/apMatrizesEsparsas/apMatrizesEsparsas/MatrizEsparsa.cs
+++ 18172_18174_Projeto1ED/apMatrizesEsparsas/apMatrizesEsparsas/MatrizEsparsa.cs
@@ -8,52 +8,5 @@ namespace apMatrizesEsparsas
 {
     class MatrizEsparsa
     {
-        int qtdLinhas, qtdColunas;
-        ListaCircularCelula[] linhas, colunas;
-
-        public ListaCircularCelula ListaLinha(int i)
-        {
-            if (i < 0 || i >= qtdLinhas)
-                throw new Exception("Índice de linha inválido");
-            return linhas[i];
-        }
-
-        public ListaCircularCelula ListaColuna(int i)
-        {
-            if (i < 0 || i >= qtdColunas)
-                throw new Exception("Índice de coluna inválido");
-            return colunas[i];
-        }
-
-        public int QtdLinhas { get => qtdLinhas; }
-
-        public int QtdColunas { get => qtdColunas; }
-
-        public MatrizEsparsa(int qtdL, int qtdC)
-        {
-
-        }
-
-        public MatrizEsparsa SomaConstante(double k)
-        {
-
-        }
-
-        public MatrizEsparsa SomaMatriz(MatrizEsparsa b)
-        {
-            if (b == null)
-                throw new Exception("Matriz B nula");
-        }
-
-        public MatrizEsparsa MultiplicacaoConstante(double k)
-        {
-
-        }
-
-        public MatrizEsparsa MultiplicacaoMatriz(MatrizEsparsa b)
-        {
-            if (b == null)
-                throw new Exception("Matriz B nula");
-        }
     }
 }
