9a84f3a4baa0d5faf03bc9428cd3e43762f4d79f
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-03-27 21:40:18 -0300

    Conserto dos métodos Construtor, ExisteCelula, Incluir e Exibir

--- 18172_18174_Projeto1ED/apMatrizesEsparsas/apMatrizesEsparsas/ListaCruzada.cs
+++ 18172_18174_Projeto1ED/apMatrizesEsparsas/apMatrizesEsparsas/ListaCruzada.cs
@@ -49,22 +49,12 @@ namespace apMatrizesEsparsas
         private bool ExisteCelula(Celula cabecaLinha, Celula cabecaColuna, ref Celula esquerda, ref Celula acima)
         {            
             esquerda = cabecaLinha;
-            for (int c = 1; c < cabecaColuna.Coluna; c++)
-            {
-                if (esquerda.Direita.Coluna == cabecaLinha.Coluna)
-                    break;
+            while (esquerda.Direita.Coluna < cabecaColuna.Coluna && esquerda.Direita.Coluna>cabecaLinha.Coluna)
                 esquerda = esquerda.Direita;
-            }
             acima = cabecaColuna;
-            for (int l = 1; l < cabecaLinha.Linha; l++)
-            {
-                if (acima.Abaixo.Linha == cabecaColuna.Linha)
-                    break;
+            while (acima.Abaixo.Linha < cabecaLinha.Linha && acima.Abaixo.Linha>cabecaColuna.Linha)
                 acima = acima.Abaixo;
-            }
-            if (esquerda.Direita.Coluna == cabecaLinha.Coluna)
-                return false;
-            return true;
+            return esquerda.Direita.Coluna == cabecaColuna.Coluna;
         }
 
         public void Incluir(Celula novaCelula)
@@ -74,52 +64,59 @@ namespace apMatrizesEsparsas
             if (novaCelula.Linha <= indCabeca || novaCelula.Linha > qtdLinhas)
                 throw new Exception("Linha da célula inválida");
             if (novaCelula.Coluna <= indCabeca || novaCelula.Coluna > qtdColunas)
-                throw new Exception("Coluna da célula inválida");
-            novaCelula.Direita = null;
-            novaCelula.Abaixo = null;
+                throw new Exception("Coluna da célula inválida");            
             Celula esquerda = null, acima = null;
             Celula cabecaLinha = cabeca;
             for (int l = 1; l <= novaCelula.Linha; l++)
                 cabecaLinha = cabecaLinha.Abaixo;
+            novaCelula.Direita = cabecaLinha;
             Celula cabecaColuna = cabeca;
             for (int c = 1; c <= novaCelula.Coluna; c++)
-                cabecaColuna = cabecaColuna.Direita;
+                cabecaColuna = cabecaColuna.Direita;            
+            novaCelula.Abaixo = cabecaColuna;
             if (ExisteCelula(cabecaLinha, cabecaColuna, ref esquerda, ref acima))
                 throw new Exception("Célula já existente");
             esquerda.Direita = novaCelula;
             acima.Abaixo = novaCelula;
-            Celula cabecaLinhaAbaixo = cabecaLinha.Abaixo;
-            Celula abaixo = cabecaLinhaAbaixo;
-            for(int c=1; c<=novaCelula.Coluna; c++)
+            if (novaCelula.Linha < qtdLinhas)
             {
-                if (abaixo.Direita.Coluna == cabecaLinhaAbaixo.Coluna)
-                    break;
-                abaixo = abaixo.Direita;
-                if (abaixo.Coluna==novaCelula.Coluna)
+                Celula cabecaLinhaAbaixo = cabecaLinha.Abaixo;
+                Celula abaixo = cabecaLinhaAbaixo;
+                do
                 {
-                    novaCelula.Abaixo = abaixo;
-                    break;
-                }
+                    abaixo = abaixo.Direita;
+                    if (abaixo.Coluna == novaCelula.Coluna)
+                    {
+                        novaCelula.Abaixo = abaixo;
+                        break;
+                    }                    
+                } while (abaixo.Direita.Coluna != cabecaLinhaAbaixo.Coluna);
             }
-            Celula cabecaColunaDireita = cabecaColuna.Direita;
-            Celula direita = cabecaColunaDireita;
-            for (int l = 1; l <= novaCelula.Linha; l++)
+            else
+                novaCelula.Abaixo = cabecaColuna;
+            if (novaCelula.Coluna < qtdColunas)
             {
-                if (direita.Abaixo.Linha== cabecaColunaDireita.Linha)
-                    break;
-                direita = direita.Abaixo;
-                if (direita.Linha== novaCelula.Linha)
+                Celula cabecaColunaDireita = cabecaColuna.Direita;
+                Celula direita = cabecaColunaDireita;
+                do
                 {
-                    novaCelula.Direita = direita;
-                    break;
-                }
+                    direita = direita.Abaixo;
+                    if (direita.Linha == novaCelula.Linha)
+                    {
+                        novaCelula.Direita = direita;
+                        break;
+                    }                    
+                } while (direita.Abaixo.Linha != cabecaColunaDireita.Linha);
             }
+            else
+                novaCelula.Direita = cabecaLinha;
         }
 
         public void Exibir(DataGridView dgv)
         {
             if (dgv == null)
                 throw new Exception("DataGridView nulo");
+            dgv.Rows.Clear();
             dgv.RowCount = qtdLinhas;
             dgv.ColumnCount = qtdColunas;
             Celula cabecaColuna = cabeca.Direita;
